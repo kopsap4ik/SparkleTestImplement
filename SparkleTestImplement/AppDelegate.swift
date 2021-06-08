5c61535cf20cb80d6bf5a32b70b5cafd9e21dbb1
Vasily Petuhov <vspetukhov@icloud.com>
2021-06-08 20:23:18 +0300

    Initial Commit

---
+++ SparkleTestImplement/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SparkleTestImplement
+//
+//  Created by Василий Петухов on 08.06.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SparkleTestImplement/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SparkleTestImplement
+//
+//  Created by Василий Петухов on 08.06.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
