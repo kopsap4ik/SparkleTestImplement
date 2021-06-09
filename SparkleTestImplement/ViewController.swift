1d8ceeca8c794e5422aa33d5d9cd737b90966a76
Vasily Petuhov <vspetukhov@icloud.com>
2021-06-09 16:21:23 +0300

    add keys

--- SparkleTestImplement/ViewController.swift
+++ SparkleTestImplement/ViewController.swift
@@ -24,8 +24,7 @@ class ViewController: NSViewController {
 
     @IBAction func checkUpdatePressBtn(_ sender: Any) {
         let updater = SUUpdater.shared()
-                updater?.feedURL = URL(string: "https://github.com/kopsap4ik/SparkleTestImplement/raw/master/SparkleTestImplement/Supporting%20Files/sparkletestcast.xml")
-                updater?.checkForUpdates(self)
+        updater?.checkForUpdates(self)
     }
     
 }
